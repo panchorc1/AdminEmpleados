72b8d1d8d5bcd0e8bd463d996a4ce4fabdfd2300
Francisco Rodríguez <panchorc1@gmail.com>
2021-05-26 11:25:21 -0600

    Se agrega una clase para hacer conexion con la base de datos.

---
+++ DAL/ConexionDAL.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminEmpleados.DAL
+{
+    class ConexionDAL
+    {
+        public bool PruebaConectar()
+        {
+            try { 
+                
+            } catch {
+            }
+        }
+    }
+}
