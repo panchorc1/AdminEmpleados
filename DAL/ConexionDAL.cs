52fe3ab5009b10f2e5c72d615d1e08c77ad0a24d
Francisco Rodríguez <panchorc1@gmail.com>
2021-05-27 18:40:12 -0600

    Metodo para ejecutar las sentencias de tipo select con Data set y Adaptador

--- DAL/ConexionDAL.cs
+++ DAL/ConexionDAL.cs
@@ -37,5 +37,24 @@ namespace AdminEmpleados.DAL
             }
         }
         /*SELECT (Retorno datos)*/
+        public DataSet EjecutarSentencia(SqlCommand sqlComando)
+        {
+            DataSet DS = new DataSet();
+            SqlDataAdapter Adaptador = new SqlDataAdapter();
+
+            try {
+                SqlCommand Comando = new SqlCommand();
+                Comando = sqlComando;
+                Comando.Connection = EstablecerConexion();
+                Adaptador.SelectCommand = Comando;
+                Conexion.Open();
+                Adaptador.Fill(DS);
+                Conexion.Close();
+                return DS;
+            }
+            catch {
+                return DS;
+            }
+        }
     }
 }
